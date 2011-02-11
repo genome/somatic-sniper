2c64ad849b40bdfb27cd625e8d559f9b9a755cdc
Travis Abbott <tabbott@genome.wustl.edu>
2011-02-11 13:20:23 -0600

    updated to use build-common instead of cmake/ subdir

--- cmake/TestHelper.cmake
+++
@@ -1,5 +0,0 @@
-macro(def_test testName)
-    add_executable(Test${testName} Test${testName}.cpp ${COMMON_SOURCES} ${EXTRA_SOURCES})
-    target_link_libraries(Test${testName} ${TEST_LIBS} ${GTEST_BOTH_LIBRARIES})
-    add_test(Test${testName} Test${testName})
-endmacro(def_test testName)
