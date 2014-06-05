fadbc15705cf88cda8b87ee2755d9871a61ac344
Dave Larson <dlarson@genome.wustl.edu>
2014-06-05 17:32:26 -0500

    refactor: make method to evaluate if a combination of reference, tumor genotype and normal genotype should be filtered

--- src/lib/sniper/allele_util.c
+++ src/lib/sniper/allele_util.c
@@ -14,3 +14,21 @@ int is_loh(int a, int b) {
 
     return ((a&b) != b);
 }
+
+/* returns 1 if the site should be filtered out because it is LOH from either the tumor or normal 
+ * R T  N
+ * A AA AG LOH
+ * A GG AG LOH
+ * A GG CG LOH
+ * A CC CG LOH
+ * A AG GG LOH not really but should be filtered
+ * A AG AA Not LOH
+ * */
+int should_filter_as_loh(int ref_base, int tumor_genotype, int normal_genotype) {
+    if (is_loh(tumor_genotype, normal_genotype) || (is_loh(normal_genotype, tumor_genotype && normal_genotype != ref_base))) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
--- src/lib/sniper/allele_util.h
+++ src/lib/sniper/allele_util.h
@@ -22,6 +22,9 @@ int count_alleles(int a);
 /* returns 1 if the alleles in a represent a loss of heterozygosity event with respect to b */
 int is_loh(int a, int b);
 
+/* returns 1 if the site should be filtered out because it is LOH from either the tumor or normal */
+int should_filter_as_loh(int ref_base, int tumor_genotype, int normal_genotype);
+
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
